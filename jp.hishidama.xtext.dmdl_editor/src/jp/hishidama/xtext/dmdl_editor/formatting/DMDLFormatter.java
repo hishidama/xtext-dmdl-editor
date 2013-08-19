aa05db1647e2b92b7651dc0fd492dadb0ee36d16
hishidama <hishi.dama@asahi.email.ne.jp>
2013-08-19 11:42:20 +0900

    ソース整形処理をDMDLFormatterUtilに移動

--- jp.hishidama.xtext.dmdl_editor/src/jp/hishidama/xtext/dmdl_editor/formatting/DMDLFormatter.java
+++ jp.hishidama.xtext.dmdl_editor/src/jp/hishidama/xtext/dmdl_editor/formatting/DMDLFormatter.java
@@ -5,11 +5,8 @@ package jp.hishidama.xtext.dmdl_editor.formatting;
 
 import jp.hishidama.xtext.dmdl_editor.services.DMDLGrammarAccess;
 
-import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
-import org.eclipse.xtext.service.AbstractElementFinder;
-import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.xbase.lib.Extension;
 
 import com.google.inject.Inject;
@@ -26,62 +23,10 @@ import com.google.inject.Inject;
 public class DMDLFormatter extends AbstractDeclarativeFormatter {
 	@Inject
 	@Extension
-	private DMDLGrammarAccess f;
+	private DMDLGrammarAccess grammarAccess;
 
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-		c.setAutoLinewrap(120);
-
-		// 丸括弧
-		for (Pair<Keyword, Keyword> pair : f.findKeywordPairs("(", ")")) {
-			c.setNoSpace().after(pair.getFirst()); // 開き括弧の直後は詰める
-			c.setNoSpace().before(pair.getSecond()); // 閉じ括弧の直前は詰める
-		}
-		// 波括弧
-		curlyBrace(c, f.getRecordTermAccess());
-		curlyBrace(c, f.getModelMappingAccess());
-		curlyBrace(c, f.getModelFoldingAccess());
-		// カンマ
-		for (Keyword comma : f.findKeywords(",")) {
-			c.setNoSpace().before(comma); // 直前は詰める
-		}
-		// セミコロン
-		for (Keyword semicolon : f.findKeywords(";")) {
-			c.setNoSpace().before(semicolon); // 直前は詰める
-			c.setLinewrap().after(semicolon); // 直後は改行する
-		}
-		// @
-		for (Keyword atmark : f.findKeywords("@")) {
-			c.setNoSpace().after(atmark); // 直後は詰める
-		}
-		// ピリオド
-		for (Keyword period : f.findKeywords(".")) {
-			c.setNoSpace().around(period); // 両側とも詰める
-		}
-
-		// descriptionの直後は改行する
-		c.setLinewrap().after(f.getDescriptionRule());
-		// attributeの直後は改行する
-		c.setLinewrap().after(f.getAttributeRule());
-		c.setNoSpace().before(f.getAttributeAccess().getLeftParenthesisKeyword_2_0());
-
-		// joinedの+の直前は改行する
-		c.setLinewrap().before(f.getJoinExpressionAccess().getPlusSignKeyword_1_0());
-
-		// モデル定義の後は1行空ける
-		c.setLinewrap(2).after(f.getModelDefinitionRule());
-
-		// コメント
-		c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
-		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
-		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
-	}
-
-	private void curlyBrace(FormattingConfig c, AbstractElementFinder rule) {
-		for (Pair<Keyword, Keyword> pair : rule.findKeywordPairs("{", "}")) {
-			c.setIndentationIncrement().after(pair.getFirst());
-			c.setLinewrap().after(pair.getFirst());
-			c.setIndentationDecrement().before(pair.getSecond());
-		}
+		DMDLFormatterUtil.configureFormatting(c, grammarAccess);
 	}
 }
