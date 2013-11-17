080d5bfbb140966a743362b46deefc5ac3565e42
hishidama <hishi.dama@asahi.email.ne.jp>
2013-11-17 12:59:53 +0900

    messageのみのコンストラクターを追加

--- jp.hishidama.xtext.dmdl_editor/src/jp/hishidama/xtext/dmdl_editor/validation/ErrorStatus.java
+++ jp.hishidama.xtext.dmdl_editor/src/jp/hishidama/xtext/dmdl_editor/validation/ErrorStatus.java
@@ -6,6 +6,10 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
 public class ErrorStatus extends Status {
+	public ErrorStatus(String message) {
+		super(IStatus.ERROR, "unknown", message);
+	}
+
 	public ErrorStatus(String message, Object... args) {
 		super(IStatus.ERROR, "unknown", MessageFormat.format(message, args));
 	}
--- jp.hishidama.xtext.dmdl_editor/src/jp/hishidama/xtext/dmdl_editor/validation/WarningStatus.java
+++ jp.hishidama.xtext.dmdl_editor/src/jp/hishidama/xtext/dmdl_editor/validation/WarningStatus.java
@@ -6,6 +6,10 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
 public class WarningStatus extends Status {
+	public WarningStatus(String message) {
+		super(IStatus.WARNING, "unknown", message);
+	}
+
 	public WarningStatus(String message, Object... args) {
 		super(IStatus.WARNING, "unknown", MessageFormat.format(message, args));
 	}
