e5285f480f7b75ff4354d850f7468cf1db6972aa
hishidama <hishi.dama@asahi.email.ne.jp>
2014-02-22 14:57:13 +0900

    CompilationUnitHyperlinkDetectorに変更

--- jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/jdt/hyperlink/OpenKeyDmdlHyperlinkDetector.java
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/jdt/hyperlink/OpenKeyDmdlHyperlinkDetector.java
@@ -1,40 +1,15 @@
 package jp.hishidama.xtext.dmdl_editor.jdt.hyperlink;
 
+import jp.hishidama.eclipse_plugin.jdt.hyperlink.CompilationUnitHyperlinkDetector;
 import jp.hishidama.xtext.dmdl_editor.dmdl.Property;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.javaeditor.WorkingCopyManager;
 import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.texteditor.ITextEditor;
 
-@SuppressWarnings("restriction")
-public class OpenKeyDmdlHyperlinkDetector extends AbstractHyperlinkDetector {
+public class OpenKeyDmdlHyperlinkDetector extends CompilationUnitHyperlinkDetector {
 
-	public IHyperlink[] detectHyperlinks(ITextViewer textViewer, IRegion region, boolean canShowMultipleHyperlinks) {
-		ITextEditor editor = (ITextEditor) getAdapter(ITextEditor.class);
-		if (editor == null) {
-			return null;
-		}
-		ICompilationUnit unit = getCompilationUnit(editor);
-		if (unit == null) {
-			return null;
-		}
-
-		return detectHyperlinks(unit, region);
-	}
-
-	protected ICompilationUnit getCompilationUnit(ITextEditor editor) {
-		IEditorInput input = editor.getEditorInput();
-		WorkingCopyManager manager = JavaPlugin.getDefault().getWorkingCopyManager();
-		return manager.getWorkingCopy(input, false);
-	}
-
-	private IHyperlink[] detectHyperlinks(ICompilationUnit unit, IRegion region) {
+	protected IHyperlink[] detectHyperlinks(ICompilationUnit unit, IRegion region) {
 		PropertyStringFinder finder = new PropertyStringFinder(unit, region.getOffset());
 		Property property = finder.getProperty();
 		if (property != null) {
