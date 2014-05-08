feb0a8e7efa500f0662a3f5f779c45013bc9ae2f
hishidama <hishi.dama@asahi.email.ne.jp>
2014-05-08 23:52:08 +0900

    Javadoc内のデータモデル名の入力補完機能を追加

---
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/jdt/assist/JavadocDmdlCompletionProposalComputer.java
@@ -0,0 +1,72 @@
+package jp.hishidama.xtext.dmdl_editor.jdt.assist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import jp.hishidama.xtext.dmdl_editor.dmdl.ModelDefinition;
+import jp.hishidama.xtext.dmdl_editor.dmdl.ModelUtil;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposalComputer;
+import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+
+public class JavadocDmdlCompletionProposalComputer implements IJavaCompletionProposalComputer {
+
+	// @Override
+	public void sessionStarted() {
+	}
+
+	// @Override
+	public List<ICompletionProposal> computeCompletionProposals(ContentAssistInvocationContext context,
+			IProgressMonitor monitor) {
+		if (!(context instanceof JavaContentAssistInvocationContext)) {
+			return Collections.emptyList();
+		}
+		JavaContentAssistInvocationContext javaContext = (JavaContentAssistInvocationContext) context;
+
+		ICompilationUnit cu = javaContext.getCompilationUnit();
+		int offset = context.getInvocationOffset();
+		JavadocDmdlFinder finder = new JavadocDmdlFinder(cu, offset);
+
+		ModelDefinition model = finder.getModel();
+		if (model == null) {
+			return Collections.emptyList();
+		}
+
+		List<ICompletionProposal> list = new ArrayList<ICompletionProposal>();
+		{
+			String s = ModelUtil.getDecodedDescription(model);
+			if (s != null) {
+				CompletionProposal proposal = new CompletionProposal(s, offset, 0, s.length());
+				list.add(proposal);
+			}
+		}
+		{
+			String s = model.getName();
+			CompletionProposal proposal = new CompletionProposal(s, offset, 0, s.length());
+			list.add(proposal);
+		}
+		return list;
+	}
+
+	// @Override
+	public List<IContextInformation> computeContextInformation(ContentAssistInvocationContext context,
+			IProgressMonitor monitor) {
+		return null;
+	}
+
+	// @Override
+	public String getErrorMessage() {
+		return null;
+	}
+
+	// @Override
+	public void sessionEnded() {
+	}
+}
---
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/jdt/assist/JavadocDmdlFinder.java
@@ -0,0 +1,141 @@
+package jp.hishidama.xtext.dmdl_editor.jdt.assist;
+
+import java.util.List;
+
+import jp.hishidama.xtext.dmdl_editor.dmdl.ModelDefinition;
+import jp.hishidama.xtext.dmdl_editor.dmdl.ModelUiUtil;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Javadoc;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.TagElement;
+import org.eclipse.jdt.core.dom.Type;
+
+public class JavadocDmdlFinder extends ASTVisitor {
+	private ICompilationUnit unit;
+	private int offset;
+
+	private String varName;
+	private String modelClassName;
+
+	public JavadocDmdlFinder(ICompilationUnit unit, int offset) {
+		this.unit = unit;
+		this.offset = offset;
+	}
+
+	private ModelDefinition model;
+
+	public ModelDefinition getModel() {
+		if (model != null) {
+			return model;
+		}
+
+		visit();
+		if (modelClassName == null) {
+			return null;
+		}
+
+		IProject project = unit.getJavaProject().getProject();
+		int n = modelClassName.indexOf('<');
+		int e = modelClassName.lastIndexOf('>');
+		if (n >= 0 && e >= 0) {
+			modelClassName = modelClassName.substring(n + 1, e);
+		}
+		this.model = ModelUiUtil.findModelByClass(project, modelClassName);
+		return model;
+	}
+
+	private boolean visited = false;
+
+	private void visit() {
+		if (visited) {
+			return;
+		}
+		visited = true;
+
+		ASTParser parser = ASTParser.newParser(AST.JLS4);
+		parser.setSource(unit);
+		parser.setSourceRange(offset, 1);
+		ASTNode node = parser.createAST(new NullProgressMonitor());
+		node.accept(this);
+	}
+
+	@Override
+	public boolean preVisit2(ASTNode node) {
+		int offset = node.getStartPosition();
+		int length = node.getLength();
+		return offset <= this.offset && this.offset <= offset + length;
+	}
+
+	@Override
+	public boolean visit(FieldDeclaration node) {
+		this.modelClassName = node.getType().toString();
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		if (javadoc(node.getJavadoc())) {
+			if ("return".equals(varName)) {
+				Type type = node.getReturnType2();
+				this.modelClassName = type.toString();
+			} else if (varName != null) {
+				List<SingleVariableDeclaration> plist = node.parameters();
+				for (SingleVariableDeclaration variable : plist) {
+					String name = variable.getName().getIdentifier();
+					if (varName.equals(name)) {
+						this.modelClassName = variable.getType().toString();
+						break;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	private boolean javadoc(Javadoc javadoc) {
+		List<TagElement> tlist = javadoc.tags();
+		for (TagElement tag : tlist) {
+			if (tag.getStartPosition() <= offset && offset <= tag.getStartPosition() + tag.getLength()) {
+				String name = tag.getTagName();
+				if ("@return".equals(name)) {
+					javadocReturn(tag);
+				} else if ("@param".equals(name)) {
+					javadocParam(tag);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void javadocReturn(TagElement tag) {
+		if (tag.getStartPosition() + "@return".length() <= offset) {
+			this.varName = "return";
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void javadocParam(TagElement tag) {
+		List<ASTNode> flist = tag.fragments();
+		if (flist.size() >= 1) {
+			ASTNode node = flist.get(0);
+			if (node.getStartPosition() + node.getLength() <= offset) {
+				if (node instanceof SimpleName) {
+					this.varName = ((SimpleName) node).getIdentifier();
+				}
+			}
+		}
+	}
+}
