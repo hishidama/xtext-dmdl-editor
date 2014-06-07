aac514dbb602840f53d0b33e7250ec89031f213b
hishidama <hishi.dama@asahi.email.ne.jp>
2014-06-07 09:22:45 +0900

    親クラスをEditDialogに変更

--- jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/dialog/DataModelPreviewDialog.java
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/dialog/DataModelPreviewDialog.java
@@ -1,37 +1,25 @@
 package jp.hishidama.xtext.dmdl_editor.ui.dialog;
 
-import org.eclipse.jface.dialogs.Dialog;
+import jp.hishidama.eclipse_plugin.dialog.EditDialog;
+
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-public class DataModelPreviewDialog extends Dialog {
-	private String windowTitle;
+public class DataModelPreviewDialog extends EditDialog {
 	private String text;
 
 	private Text textArea;
 
 	public DataModelPreviewDialog(Shell parentShell, String windowTitle) {
-		super(parentShell);
-		setShellStyle(getShellStyle() | SWT.RESIZE);
-		this.windowTitle = windowTitle;
+		super(parentShell, windowTitle, 1);
 	}
 
 	@Override
-	public void create() {
-		super.create();
-
-		getShell().setText(windowTitle);
-	}
-
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		Composite composite = (Composite) super.createDialogArea(parent);
-
+	protected void createFields(Composite composite) {
 		textArea = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.READ_ONLY | SWT.H_SCROLL | SWT.V_SCROLL);
 		// textArea.setFont(composite.getFont());
 		GridData grid = new GridData(GridData.FILL_BOTH);
@@ -41,8 +29,6 @@ public class DataModelPreviewDialog extends Dialog {
 		if (text != null) {
 			textArea.setText(text);
 		}
-
-		return composite;
 	}
 
 	public void setText(String text) {
@@ -50,6 +36,17 @@ public class DataModelPreviewDialog extends Dialog {
 		if (textArea != null) {
 			textArea.setText(text);
 		}
+	}
+
+	@Override
+	protected void refresh() {
+		// do nothing
+	}
+
+	@Override
+	protected boolean validate() {
+		// do nothing
+		return true;
 	}
 
 	@Override
--- jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/wizard/page/datamodel/ModifyPropertyAttributeDialog.java
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/wizard/page/datamodel/ModifyPropertyAttributeDialog.java
@@ -2,47 +2,35 @@ package jp.hishidama.xtext.dmdl_editor.ui.wizard.page.datamodel;
 
 import java.io.StringReader;
 
+import jp.hishidama.eclipse_plugin.dialog.EditDialog;
 import jp.hishidama.eclipse_plugin.util.StringUtil;
 import jp.hishidama.xtext.dmdl_editor.parser.antlr.DMDLParser;
 import jp.hishidama.xtext.dmdl_editor.services.DMDLGrammarAccess;
 import jp.hishidama.xtext.dmdl_editor.ui.internal.InjectorUtil;
 import jp.hishidama.xtext.dmdl_editor.util.DMDLStringUtil;
 
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.parser.IParseResult;
 
-public class ModifyPropertyAttributeDialog extends Dialog {
+public class ModifyPropertyAttributeDialog extends EditDialog {
 
 	private DataModelRow row;
 	private Text attributeText;
 
 	public ModifyPropertyAttributeDialog(Shell parentShell, DataModelRow row) {
-		super(parentShell);
-		setShellStyle(getShellStyle() | SWT.RESIZE);
+		super(parentShell, "プロパティー属性編集");
 		this.row = row;
 	}
 
 	@Override
-	protected Control createDialogArea(Composite parent) {
-		getShell().setText("プロパティー属性編集");
-
-		Composite composite = (Composite) super.createDialogArea(parent);
-		GridLayout layout = (GridLayout) composite.getLayout();
-		layout.numColumns = 2;
-
+	protected void createFields(Composite composite) {
 		addField(composite, "name", row.getName(), false);
 		addField(composite, "description", row.getDescription(), false);
 		addField(composite, "type", row.getDataType(), false);
@@ -51,28 +39,25 @@ public class ModifyPropertyAttributeDialog extends Dialog {
 		grid.widthHint = 256 + 128;
 		grid.heightHint = 20 * 6;
 		attributeText.setLayoutData(grid);
-		attributeText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				validateParse(attributeText);
-			}
-		});
 		attributeText.setFocus();
-
-		return composite;
 	}
 
 	private Text addField(Composite composite, String label, String text, boolean edit) {
-		new Label(composite, SWT.NONE).setText(label);
+		createLabel(composite, label);
 
 		int style = SWT.BORDER;
 		if (edit) {
 			style |= SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL;
 		}
-		Text t = new Text(composite, style);
+		Text t = createText(composite, style);
 		t.setText(StringUtil.nonNull(text));
-		t.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		t.setEditable(edit);
 		return t;
+	}
+
+	@Override
+	protected boolean validate() {
+		return validateParse(attributeText);
 	}
 
 	private DMDLParser parser;
@@ -116,6 +101,11 @@ public class ModifyPropertyAttributeDialog extends Dialog {
 		}
 		text.setToolTipText(null);
 		return true;
+	}
+
+	@Override
+	protected void refresh() {
+		refreshOkButton();
 	}
 
 	@Override
