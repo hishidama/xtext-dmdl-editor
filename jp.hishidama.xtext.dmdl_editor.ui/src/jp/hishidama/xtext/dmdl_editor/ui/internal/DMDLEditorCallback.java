64407ae4bd4c2e88b144c2b29dac1e0379b17e2d
hishidama <hishi.dama@asahi.email.ne.jp>
2014-10-18 14:26:24 +0900

    fixed #44. DMDLEditorCallbackの親クラスをNatureAddingEditorCallbackに変更

--- jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/internal/DMDLEditorCallback.java
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/internal/DMDLEditorCallback.java
@@ -1,45 +1,37 @@
 package jp.hishidama.xtext.dmdl_editor.ui.internal;
 
-import java.util.Arrays;
-
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.xtext.ui.XtextProjectHelper;
-import org.eclipse.xtext.ui.editor.AbstractDirtyStateAwareEditorCallback;
+import org.eclipse.xtext.builder.nature.NatureAddingEditorCallback;
+import org.eclipse.xtext.builder.nature.ToggleXtextNatureAction;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 
-public class DMDLEditorCallback extends AbstractDirtyStateAwareEditorCallback {
+import com.google.inject.Inject;
+
+@SuppressWarnings("restriction")
+public class DMDLEditorCallback extends NatureAddingEditorCallback {
+
+	@Inject
+	private ToggleXtextNatureAction toggleNature;
 
 	@Override
 	public void afterCreatePartControl(XtextEditor editor) {
-		super.afterCreatePartControl(editor);
-
 		IResource resource = editor.getResource();
-		if (resource == null) {
-			return;
+		if (resource != null) {
+			IProject project = resource.getProject();
+			if (!hasNature(project) && project.isAccessible() && !project.isHidden()) {
+				addNature(project);
+			}
 		}
-		IProject project = resource.getProject();
-		if (!hasNature(project) && project.isAccessible() && !project.isHidden()) {
-			addNature(project);
-		}
+
+		super.afterCreatePartControl(editor);
 	}
 
-	public static boolean hasNature(IProject project) {
-		return XtextProjectHelper.hasNature(project);
+	public boolean hasNature(IProject project) {
+		return toggleNature.hasNature(project);
 	}
 
-	public static void addNature(IProject project) {
-		try {
-			IProjectDescription description = project.getDescription();
-			String[] natures = description.getNatureIds();
-			String[] newNatures = Arrays.copyOf(natures, natures.length + 1);
-			newNatures[natures.length] = XtextProjectHelper.NATURE_ID;
-			description.setNatureIds(newNatures);
-			project.setDescription(description, null);
-		} catch (CoreException e) {
-			LogUtil.log(e.getStatus());
-		}
+	public void addNature(IProject project) {
+		toggleNature.toggleNature(project);
 	}
 }
