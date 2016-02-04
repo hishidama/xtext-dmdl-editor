4aed5c6206c3aa3e832fa4528409e03ef4fc8e86
hishidama <hishi.dama@asahi.email.ne.jp>
2016-02-04 22:32:46 +0900

    DMDLBuilderParticipant: Xtext2.9対応

--- jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/builder/DMDLBuilderParticipant.java
+++ jp.hishidama.xtext.dmdl_editor.ui/src/jp/hishidama/xtext/dmdl_editor/ui/builder/DMDLBuilderParticipant.java
@@ -1,18 +1,32 @@
 package jp.hishidama.xtext.dmdl_editor.ui.builder;
 
-import jp.hishidama.xtext.dmdl_editor.generator.DMDLGenerator;
+import java.lang.reflect.Method;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.xtext.builder.BuilderParticipant;
+import org.eclipse.xtext.generator.IGenerator;
+
+import jp.hishidama.xtext.dmdl_editor.generator.DMDLGenerator;
 
 public class DMDLBuilderParticipant extends BuilderParticipant {
 
 	@Override
 	public void build(final IBuildContext context, IProgressMonitor monitor) throws CoreException {
-		DMDLGenerator gen = (DMDLGenerator) getGenerator();
-		long fullTime = (context.getBuildType() == BuildType.FULL) ? System.currentTimeMillis() : 0;
-		gen.setFullTime(fullTime);
+		IGenerator gen = getGenerator();
+		if (!(gen instanceof DMDLGenerator)) {
+			try {
+				// Xtext2.9
+				Method method = gen.getClass().getMethod("getLegacyGenerator");
+				gen = (IGenerator) method.invoke(gen);
+			} catch (Exception e) {
+				// fall through
+			}
+		}
+		if (gen instanceof DMDLGenerator) {
+			long fullTime = (context.getBuildType() == BuildType.FULL) ? System.currentTimeMillis() : 0;
+			((DMDLGenerator) gen).setFullTime(fullTime);
+		}
 
 		super.build(context, monitor);
 	}
